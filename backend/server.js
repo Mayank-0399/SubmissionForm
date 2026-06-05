8d05167962e3cf3e2a64b7d708b418bcdf04e176
Mayank-0399 <mayanksingh230651@gmail.com>
2026-06-06 01:30:35 +0530

    Fix

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.get("/", (req, res) => {
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 // Catch-all route for SPA
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
